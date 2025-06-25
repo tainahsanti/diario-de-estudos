b1d45d67e2d39dfacac1b27f400da2407085fd82
Tainah Santi <132309252+tainahsanti@users.noreply.github.com>
2025-06-25 10:35:34 -0300

    Add files via upload

---
+++ faculdade/python/2025-05-26_aula2/exercicio5_casasdecimais.py
@@ -0,0 +1,7 @@
+#Exercicio CHATGPT
+
+# Exercício 5 — Formatando saída com casas decimais
+#Receba um valor em reais e mostre com duas casas decimais:
+
+valor = float(input("Digite um valor em reais: "))
+print(f"O valor digitado foi R$ {valor:.2f}")
